84a687458732e9c94bb757d537bfa70658edc111
thobela <thobela.sixpence@gmail.com>
2025-05-12 21:50:37 +0200

    new: added new code for data configs of each step

---
+++ src/entity/config_entity.py
@@ -0,0 +1,22 @@
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
+from typing import List, Dict
+
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+load_dotenv(os.path.join(root_dir, ".env"))
+
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path: str = "data/raw/train.csv"
+
+
+@dataclass
+class DataPreprocessingConfig:
+    processed_data_path: str = "data/processed_data/"
+
+
+@dataclass
+class ModelTrainingConfig:
+    model_artifact_dir: str = "src/models/artifacts/"
