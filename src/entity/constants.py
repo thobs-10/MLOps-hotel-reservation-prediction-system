e38c21d60cde46d3beeb91548af5c1afba00802d
thobela <thobela.sixpence@gmail.com>
2025-06-15 01:37:15 +0200

    new: code for engineering the columns

--- src/entity/constants.py
+++ src/entity/constants.py
@@ -1,1 +1,34 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class FeatureEngineeringConstants(BaseModel):
+    """
+    Configuration for feature engineering.
+    """
+
+    categorical_columns: List[str] = [
+        "type_of_meal_plan",
+        "room_type_reserved",
+        "market_segment_type",
+    ]
+    numerical_columns: List[str] = [
+        "lead_time",
+        "no_of_weekend_nights",
+        "no_of_week_nights",
+    ]
+    target_column: str = "booking_status"
+    new_features: List[str] = ["total_nights_stayed", "is_repeat_guest"]
+    label_encoder_columns: List[str] = [
+        "type_of_meal_plan",
+        "room_type_reserved",
+        "market_segment_type",
+    ]
+    pca_components: int = 2
+
+
+# class DataPreprocessingConstants(BaseModel):
+#     """Irrelevant columns for data preprocessing."""
+
 irrelevant_columns: str = "Booking_ID"
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -1,6 +1,10 @@
-from typing import List
+from typing import List, Tuple
 
+import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.preprocessing import LabelEncoder
 
 
 def get_categorical_columns(df: pd.DataFrame) -> List[str]:
@@ -21,3 +25,44 @@ def get_numerical_columns(df: pd.DataFrame) -> List[str]:
         include=["int64", "float64"]
     ).columns.tolist()
     return numerical_columns
+
+
+def generate_label_encoder() -> LabelEncoder:
+    """
+    Generate a LabelEncoder instance for encoding categorical variables.
+    Returns:
+        LabelEncoder: Instance of LabelEncoder.
+    """
+    label_encoder = LabelEncoder()
+    return label_encoder
+
+
+def create_feature_importance_selector(
+    X: pd.DataFrame, y: pd.Series
+) -> ExtraTreesClassifier:
+    """
+    Create a feature importance selector using ExtraTreesClassifier.
+    Args:
+        X (pd.DataFrame): Features DataFrame.
+        y (pd.Series): Target variable Series.
+    Returns:
+        ExtraTreesClassifier: Fitted ExtraTreesClassifier instance.
+    """
+    if X.empty or y.empty:
+        raise ValueError("Features DataFrame or target variable Series is empty.")
+    selector = ExtraTreesClassifier(n_estimators=100)
+    selector.fit(X, y)
+    return selector
+
+
+def fit_pca(
+    X: pd.DataFrame, columns: List[str], n_components: int = 4
+) -> Tuple[PCA, np.ndarray]:
+    """
+    Fit PCA on selected columns of X.
+    Returns the fitted PCA object and the transformed X.
+    """
+    pca = PCA(n_components=n_components)
+    X_selected = X[columns]
+    X_pca = pca.fit_transform(X_selected)
+    return pca, X_pca
