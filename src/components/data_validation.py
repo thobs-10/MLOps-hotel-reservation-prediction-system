a60979c996245063c495264c323cdcc59c513565
thobela <thobela.sixpence@gmail.com>
2025-06-24 18:24:02 +0200

    fix: ruff;s isort changed the order of imports

--- src/components/data_validation.py
+++ src/components/data_validation.py
@@ -1,6 +1,6 @@
-import pandera as pa
-from pandera import Column, DataFrameSchema, Check
 import pandas as pd
+import pandera as pa
+from pandera import Check, Column, DataFrameSchema
 
 
 def no_duplicates(df: pd.DataFrame) -> bool:
